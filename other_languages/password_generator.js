d17b2ee51cb7cc481fb62e6bbb83071036f9b944
Neal Lindsay <neal.lindsay@gmail.com>
2011-06-04 09:23:19 -0400

    improve javascript version

--- other_languages/password_generator.js
+++ other_languages/password_generator.js
@@ -1,31 +1,23 @@
-/*
-substitutions
+function substitute(original_word) {
+  var word_as_array = original_word.split('');
+  var new_string = "";
 
-a e i o u 
-4 3 1 0 2
-
-password we are using 'whoami'
-our goal is:          'wh04m1'
-*/
-
-var original_word, word_as_array, x, character, new_string;
-
-original_word = 'together';
-word_as_array = original_word.split('');
-
-x = 0
-new_string = "";
-for (; x < original_word.length;) {
-  character = word_as_array[x];
-  if (character == 'o') {
-    new_string += '0';
-  } else if (character == 'e') {
-    new_string += '3';
-  } else {
-    new_string += character;
+  for (var x = 0; x < original_word.length; x++) {
+    var character = word_as_array[x];
+    if (character == 'a') {
+      new_string += '4';
+    } else if (character == 'e') {
+      new_string += '3';
+    } else if (character == 'i') {
+      new_string += '1';
+    } else if (character == 'o') {
+      new_string += '0';
+    } else if (character == 'u') {
+      new_string += '2';
+    } else {
+      new_string += character;
+    }
   }
-
-  x = x + 1;
+  
+  return new_string;
 }
-
-alert("The new string is " + new_string);
