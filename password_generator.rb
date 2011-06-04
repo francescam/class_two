5cc8ed689d902b4c2cd37f2826ef367e2c05fc40
Joe O'Brien <joe@edgecase.com>
2011-06-04 12:10:42 -0400

    adding in password_generator_test as unit test

--- password_generator.rb
+++ password_generator.rb
@@ -1,4 +1,3 @@
-
 # substitutions
 #
 # a e i o u 
@@ -6,33 +5,24 @@
 #
 # password we are using 'whoami'
 # our goal is:          'wh04m1'
-
-original_word = 'together'
-word_as_array = original_word.split('')
-
-x = 0
-new_string = ""
-while x < original_word.length
-  character = word_as_array[x]
-  if character == 'o'
-    new_string << '0'
-  elsif character == 'e'
-    new_string << '3'
-  else
-    new_string << character
-  end
+#
+def substitute(original_word)
+  word_as_array = original_word.split('')
+  substituted_word = ""
   
-  x = x + 1
+  original_word.length.times do |x|
+    character = word_as_array[x]
+    case character
+    when 'a' ; substituted_word << '5'
+    when 'e' ; substituted_word << '3'
+    when 'i' ; substituted_word << '1'
+    when 'o' ; substituted_word << '0'
+    when 'u' ; substituted_word << '2'                      
+    else
+      substituted_word << character
+    end
+  end
+  substituted_word
 end
-
-puts "The new string is #{new_string}"
-
-
-
-
-
-
-
-
 
 
